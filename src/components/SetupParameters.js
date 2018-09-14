612686bb9dac59bc4d485c3b5894475a49bc354f
Zhukov <zulus23@gamil.com>
2018-09-14 21:10:35 +0300

    initialize project

---
+++ src/components/SetupParameters.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SetupParameters extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default SetupParameters;
